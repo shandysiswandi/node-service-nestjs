026cbbc1d4964909967d0b43310dd51f244352d1
Shandy Siswandi <shandysiswandi@gmail.com>
2021-03-21 21:20:53 +0700

    create response interceptor

---
+++ src/interceptors/response.interceptor.ts
@@ -0,0 +1,26 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface Response<T> {
+  data: T;
+  message: string;
+}
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(_: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((result) => ({
+        error: false,
+        message: result.message,
+        data: result.data,
+      })),
+    );
+  }
+}
