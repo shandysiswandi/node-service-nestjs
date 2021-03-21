123da9c8ada2f262860b5fe9e1b523a96d0ec3a1
Shandy Siswandi <shandysiswandi@gmail.com>
2021-03-21 20:22:12 +0700

    use global middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // global middlewares
+  app.enableCors();
+  app.use(helmet());
+
   await app.listen(process.env.PORT);
   console.log(`Application is running on port: ${process.env.PORT}`);
 }
