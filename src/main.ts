b7d1e53ca214c3942a665328682f9cfb22143165
Shandy Siswandi <shandysiswandi@gmail.com>
2021-03-22 12:40:48 +0700

    add pipi validation & interceptor & middleware compresion

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
 import * as helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +11,13 @@ async function bootstrap() {
   // global middlewares
   app.enableCors();
   app.use(helmet());
+  app.use(compression());
+
+  // global pipe for validation data request
+  app.useGlobalPipes(new ValidationPipe());
+
+  // global interceptor for manipulate req and res
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   await app.listen(process.env.PORT);
   console.log(`Application is running on port: ${process.env.PORT}`);
