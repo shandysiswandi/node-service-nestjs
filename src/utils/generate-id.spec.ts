2707e938f79dbbd9c12666225fc584d086de666a
Shandy Siswandi <shandysiswandi@gmail.com>
2021-03-23 21:55:03 +0700

    update generate-id.spec.ts

--- src/utils/generate-id.spec.ts
+++ src/utils/generate-id.spec.ts
@@ -2,21 +2,21 @@ import { ID } from './generate-id';
 
 describe('Generate ID', () => {
   describe('ID()', () => {
-    it('should return an string with length 5', async () => {
+    it('should return an string with length 5', () => {
       const actual = ID(5);
       const expected = 5;
 
       expect(actual.length).toBe(expected);
     });
 
-    it('should return an string with length 10', async () => {
+    it('should return an string with length 10', () => {
       const actual = ID(10);
       const expected = 10;
 
       expect(actual.length).toBe(expected);
     });
 
-    it('should return an string with length 21 (default)', async () => {
+    it('should return an string with length 21 (default)', () => {
       const actual = ID();
       const expected = 21;
 
