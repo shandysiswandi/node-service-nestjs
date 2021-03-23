f5ddb8cfae6ed345a94257bb5c08890409fbc7b1
Shandy Siswandi <shandysiswandi@gmail.com>
2021-03-23 21:54:37 +0700

    update mail.ts

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './interceptors/response.interceptor';
 import * as helmet from 'helmet';
 import * as compression from 'compression';
+import {
+  HttpStatus,
+  UnprocessableEntityException,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ValidationError } from 'class-validator';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: false,
+    cors: true,
+  });
+
+  // routes prefix
+  // app.setGlobalPrefix(process.env.NAME);
 
   // global middlewares
-  app.enableCors();
   app.use(helmet());
   app.use(compression());
 
-  // global pipe for validation data request
-  app.useGlobalPipes(new ValidationPipe());
+  // global filter
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   // global interceptor for manipulate req and res
   app.useGlobalInterceptors(new ResponseInterceptor());
+
+  // global pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: false,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      transform: true,
+      dismissDefaultMessages: false,
+      exceptionFactory: (errors: ValidationError[]) =>
+        new UnprocessableEntityException(errors),
+    }),
+  );
 
   await app.listen(process.env.PORT);
   console.log(`Application is running on port: ${process.env.PORT}`);
