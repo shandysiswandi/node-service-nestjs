06428bd4c443faf0b2326715ad3f2fba9f1b620c
Shandy Siswandi <shandysiswandi@gmail.com>
2021-03-23 21:54:20 +0700

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigurationModule } from './config/configuration.module';
-import { DatabaseModule } from './config/database.module';
+import { DatabaseModule } from './database/database.module';
 import { UserModule } from './domain/user/user.module';
 
 @Module({
   imports: [ConfigurationModule, DatabaseModule, UserModule],
-  controllers: [],
   providers: [],
+  controllers: [],
 })
 export class AppModule {}
