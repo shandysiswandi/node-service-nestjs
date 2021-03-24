95f0e457cadec4123f97ee2219d97694fcaf84c2
Shandy Siswandi <shandysiswandi@gmail.com>
2021-03-24 23:22:02 +0700

    deleted unused

--- src/utils/generate-id.ts
+++
@@ -1,5 +0,0 @@
-import { nanoid } from 'nanoid';
-
-export function ID(size?: number) {
-  return nanoid(size);
-}
--- src/utils/response.ts
+++
@@ -1,6 +0,0 @@
-export async function response(message: string, data: any): Promise<any> {
-  return {
-    message: message,
-    data: data,
-  };
-}
